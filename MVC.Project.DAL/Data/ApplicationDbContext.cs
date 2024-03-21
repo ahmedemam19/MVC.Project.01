d384005615b85f0b2a1e36d91bc600fa9aa50c1c
asus <asus@AHMED-EMAM>
2024-03-21 20:25:42 +0200

    Generate Migration

--- MVC.Project.DAL/Data/ApplicationDbContext.cs
+++ MVC.Project.DAL/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MVC.Project.DAL.Data
 {
-    internal class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
