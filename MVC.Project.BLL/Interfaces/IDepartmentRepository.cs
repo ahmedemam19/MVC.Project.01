93ad03f02ca865bb6cd279b914fa63375b05bbce
asus <asus@AHMED-EMAM>
2024-03-21 21:09:00 +0200

    3-Layers Arch. Pattern - Business Logic Layer

---
+++ MVC.Project.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using MVC.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Project.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department Get(int id);
+
+        int Add(Department entity);
+
+        int Update(Department entity);
+        
+        int Delete(Department entity);
+
+    }
+}
---
+++ MVC.Project.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Project.BLL.Interfaces;
+using MVC.Project.DAL.Data;
+using MVC.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Project.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DepartmentRepository(ApplicationDbContext dbContext) // Ask CLR for Creating Object from "ApplicationDbContext"
+        {
+            //_dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>);
+            _dbContext = dbContext;
+        }
+
+        public int Add(Department entity)
+        {
+            _dbContext.departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Department entity)
+        {
+            _dbContext.departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+        
+
+        public Department Get(int id)
+        {
+            ///var department = _dbContext.departments.Local.Where(d => d.Id == id).FirstOrDefault();
+            ///if(department == null)
+            ///    department = _dbContext.departments.Local.Where(d => d.Id == id).FirstOrDefault();
+            ///return department;
+
+            //return _dbContext.departments.Find(id);
+
+            return _dbContext.Find<Department>(id); // EF Core 3.1 New Feature
+        }
+
+        public IEnumerable<Department> GetAll()
+            => _dbContext.departments.AsNoTracking().ToList();
+
+
+
+
+    }
+}
