f32df49dd88a05dc41be1ee2f63e6bc75168c1d7
asus <asus@AHMED-EMAM>
2024-03-21 16:42:17 +0200

    AppSettings - Connection String

--- MVC.Project.PL/Startup.cs
+++ MVC.Project.PL/Startup.cs
@@ -18,12 +18,12 @@ namespace MVC.Project.PL
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the Dependancy Injection container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +32,7 @@ namespace MVC.Project.PL
 
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True; MultipleActiveResultSets = False");
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection"));
             });
 
         }
