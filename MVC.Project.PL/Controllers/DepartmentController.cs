1fbacc7f253fd08c3e317b2c5309d0ef95bb06b8
asus <asus@AHMED-EMAM>
2024-03-21 23:39:04 +0200

    Department Controller - Create

--- MVC.Project.PL/Controllers/DepartmentController.cs
+++ MVC.Project.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC.Project.BLL.Interfaces;
 using MVC.Project.BLL.Repositories;
+using MVC.Project.DAL.Models;
 
 namespace MVC.Project.PL.Controllers
 {
@@ -23,6 +24,25 @@ namespace MVC.Project.PL.Controllers
         {
             var departments = _deparmtentsRepo.GetAll();
             return View(departments);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if(ModelState.IsValid) // Server side validation 
+            {
+               var count = _deparmtentsRepo.Add(department);
+                if(count > 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(department);
         }
     }
 }
