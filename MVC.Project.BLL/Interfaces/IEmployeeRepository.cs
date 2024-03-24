702c6b18af61a6530d702a582c7babf8ccad1a9f
asus <asus@AHMED-EMAM>
2024-03-24 17:03:29 +0200

    Employee Repository - BLL

---
+++ MVC.Project.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using MVC.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Project.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee Get(int id);
+
+        int Add(Employee entity);
+
+        int Update(Employee entity);
+
+        int Delete(Employee entity);
+
+    }
+}
---
+++ MVC.Project.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Project.BLL.Interfaces;
+using MVC.Project.DAL.Data;
+using MVC.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Project.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public EmployeeRepository(ApplicationDbContext dbContext) // Ask CLR for Creating Object from "ApplicationDbContext"
+        {
+            //_dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>);
+            _dbContext = dbContext;
+        }
+
+        public int Add(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Employees.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Employees.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+
+        public Employee Get(int id)
+        {
+            return _dbContext.Find<Employee>(id);
+        }
+
+
+        public IEnumerable<Employee> GetAll()
+            => _dbContext.Employees.AsNoTracking().ToList();
+
+    }
+}
